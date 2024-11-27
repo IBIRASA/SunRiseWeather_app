546d440bdd5660936d2db6f2402399aa8094aaa4
IBIRASA <d.ishimwe@alustudent.com>
2024-11-27 11:51:17 +0200

    splashscreen

---
+++ sunriseweatherapp/lib/screens/splash_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import '../screens/welcome_screen.dart';
+class SplashScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Future.delayed(Duration(seconds: 5), () {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => WelcomeScreen()),
+        )
+    })
+    return  Scaffold(
+      backgroundColor: const Color.fromARGB(255, 8, 39, 75),
+      body:Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('assets/logo.png'),
+            SizedBox(height: 20),
+            CircularProgressIndicator(),
+          ],
+        ),
+      ),
+    );
+  }
+}
