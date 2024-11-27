580c8bc88769be89f2e693e176690d2523c18f19
IBIRASA <d.ishimwe@alustudent.com>
2024-11-27 11:13:03 +0200

    changed the maindart

--- sunriseweatherapp/lib/screens/welcome_screen.dart
+++ sunriseweatherapp/lib/screens/welcome_screen.dart
@@ -1,29 +1,10 @@
 import 'package:flutter/material.dart';
-import '../screens/location_search.dart';
-import '../screens/splash_screen.dart';
-import '../screens/welcome_screen.dart';
-import '../screens/signin.dart';
-import '../screens/signup.dart';
 
-void main() {
-  runApp(MyApp());
-}
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
 
-class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'SunRise app',
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-      ),
-      initialRoute: '/',
-      routes: {
-        '/': (context) => SplashScreen(),
-        '/welcome': (context) => WelcomeScreen(),
-        '/signup': (context) => SignUpScreen(),
-        '/signin': (context) => SignInScreen(),
-      },
-    );
+    return const Placeholder();
   }
 }
